bb15a60e418daae6bd2c2ae4478b2160b6474cab
I_am_Filipo <56951865+FilipooSVK@users.noreply.github.com>
2023-10-30 19:08:17 +0100

    Update pihole-update-automation.sh
    Updated script with check if cron job already exist if not script will create cron job.

--- scripts/pihole-update-automation.sh
+++ scripts/pihole-update-automation.sh
@@ -7,19 +7,30 @@ echo "Updating Pi-hole..."
 pihole -up
 
 # Path to the weekly update script
-WEEKLY_UPDATE_SCRIPT="/usr/local/bin/pihole-weekly-update.sh"
+WEEKLY_UPDATE_SCRIPT="/home/usr/scripts/pihole-weekly-update.sh" # Modify followed path where you saved script
 
-# Create the weekly update script
-echo '#!/bin/bash' > "$WEEKLY_UPDATE_SCRIPT"
-echo 'pihole -up' >> "$WEEKLY_UPDATE_SCRIPT"
-chmod +x "$WEEKLY_UPDATE_SCRIPT"
+# Create the weekly update script if it doesn't exist
+if [ ! -f "$WEEKLY_UPDATE_SCRIPT" ]; then
+  echo '#!/bin/bash' > "$WEEKLY_UPDATE_SCRIPT"
+  echo 'pihole -up' >> "$WEEKLY_UPDATE_SCRIPT"
+  chmod +x "$WEEKLY_UPDATE_SCRIPT"
+  echo "Weekly update script created."
+else
+  echo "Weekly update script already exists."
+fi
 
 # Define the schedule for the weekly update in the cron format (Friday at 3 AM)
 CRON_SCHEDULE="0 3 * * 5"
 
-# Add the cron job
-(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE $WEEKLY_UPDATE_SCRIPT") | crontab -
+# Check if the cron job already exists
+if (crontab -l 2>/dev/null | grep -q "$WEEKLY_UPDATE_SCRIPT"); then
+  echo "Cron job already exists."
+else
+  # Add the cron job
+  (crontab -l 2>/dev/null; echo "$CRON_SCHEDULE $WEEKLY_UPDATE_SCRIPT") | crontab -
+  echo "Cron job has been added."
+fi
 
 # Inform the user
-echo "Pi-hole has been updated, and a weekly update script and cron job have been created."
+echo "Pi-hole has been updated, and a weekly update script and cron job have been managed."
 
