256456dd46e7cd27726b9e30580b61fa0303e39c
I_am_Filipo <56951865+FilipooSVK@users.noreply.github.com>
2023-10-30 18:49:47 +0100

    Update pihole-backup.sh
    Changing of commands due the showing error once backup is completed.

--- scripts/pihole-backup.sh
+++ scripts/pihole-backup.sh
@@ -3,25 +3,21 @@
 # Pi-hole Backup Script
 
 # Configuration
-BACKUP_DIR="/home/usr/backup_Pihole" # Updated backup directory
+BACKUP_DIR="/home/usr/backup_Pihole" # Change the location of your backup file
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-BACKUP_FILE="${BACKUP_DIR}/pihole-teleporter-backup-${TIMESTAMP}.tar.gz"
 
-# Check if the backup directory exists, and create it if not
-if [ ! -d "$BACKUP_DIR" ]; then
-  mkdir -p "$BACKUP_DIR"
-fi
-
-# Run the Pi-hole Teleporter tool to create a backup
+# Run the Pi-hole Teleporter tool to create a backup in the default directory
 echo "Creating a Pi-hole backup using Teleporter..."
 pihole -a -t
 
-# Move the generated Teleporter backup to the specified directory
-mv /etc/pihole/teleporter/${TIMESTAMP}.tar.gz "$BACKUP_FILE"
+# Find the name of the generated Teleporter backup file in the script's directory
+BACKUP_FILE=$(find "$(dirname "$0")" -name "*.tar.gz" -type f)
 
-# Check if the backup was successfully created and moved
+# Check if the backup file exists
 if [ -f "$BACKUP_FILE" ]; then
-  echo "Pi-hole backup created and saved to: $BACKUP_FILE"
+  # Move the generated Teleporter backup to the specified directory
+  mv "$BACKUP_FILE" "$BACKUP_DIR/"
+  echo "Pi-hole backup created and saved to: $BACKUP_DIR/$(basename $BACKUP_FILE)"
 else
   echo "Pi-hole backup failed."
 fi
