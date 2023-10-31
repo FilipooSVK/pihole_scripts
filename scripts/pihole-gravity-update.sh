434d3d857b5335c0b5f713296dabc16212c6548f
I_am_Filipo <56951865+FilipooSVK@users.noreply.github.com>
2023-10-31 19:04:47 +0100

    Update pihole-gravity-update.sh

--- scripts/pihole-gravity-update.sh
+++ scripts/pihole-gravity-update.sh
@@ -3,19 +3,22 @@
 # Script to update Pi-hole's Gravity list and create a weekly cron job
 
 # Path to the Pi-hole Gravity update script
-PIHOLE_UPDATE_SCRIPT="/usr/local/bin/pihole-gravity-update.sh"
+PIHOLE_UPDATE_SCRIPT="/usr/local/bin/pihole-gravity-update.sh" # Modify path where you saved the script
 
-# Create the Gravity update script
-echo '#!/bin/bash' > "$PIHOLE_UPDATE_SCRIPT"
-echo 'pihole -g' >> "$PIHOLE_UPDATE_SCRIPT"
-chmod +x "$PIHOLE_UPDATE_SCRIPT"
-
-# Define the schedule for the weekly update in the cron format (Friday at 3 AM)
-CRON_SCHEDULE="0 3 * * 5"
-
-# Add the cron job
-(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE $PIHOLE_UPDATE_SCRIPT") | crontab -
-
-# Inform the user
-echo "Pi-hole Gravity update script and weekly cron job have been created."
-
+# Check if the cron job already exists
+if (crontab -l 2>/dev/null | grep -q "$PIHOLE_UPDATE_SCRIPT"); then
+  echo "Cron job already exists."
+else
+  # Create the Gravity update script
+  echo '#!/bin/bash' > "$PIHOLE_UPDATE_SCRIPT"
+  echo 'pihole -g' >> "$PIHOLE_UPDATE_SCRIPT"
+  chmod +x "$PIHOLE_UPDATE_SCRIPT"
+  
+  # Define the schedule for the weekly update in the cron format (Friday at 3 AM)
+  CRON_SCHEDULE="0 3 * * 5"
+  
+  # Add the cron job
+  (crontab -l 2>/dev/null; echo "$CRON_SCHEDULE $PIHOLE_UPDATE_SCRIPT") | crontab -
+  
+  echo "Pi-hole Gravity update script and weekly cron job have been created."
+fi
